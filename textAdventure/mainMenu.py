17a78b1c6f9063c435932c3ba66d4aa2c8f9ce56
Grace Egglestone <g.egglestone@surreyschools.ca>
2022-10-27 12:17:53 -0700

    4

--- textAdventure/mainMenu.py
+++ textAdventure/mainMenu.py
@@ -1,8 +1,24 @@
 # Main adventure menu
 from gameCreation import newGame
+from saveData import loadGame
 
-menuInput = int(input("Welcome to: PLACEHOLDER_NAME! please select an option below! \n "
-+ "press 1 for a new game \n press 2 to load a game \n press 3 to quit"))
+validInput = False
 
-if menuInput == 1:
-    newGame()
+while not validInput:
+    menuInput = int(input("Welcome to: PLACEHOLDER_NAME! please select an option below! \n "
+    + "press 1 for a new game \n press 2 to load a game \n press 3 to quit"))
+
+    if menuInput == 1:
+        newGame()
+        validInput = True
+
+    elif menuInput == 2:
+        loadGame()
+        validInput = True
+
+    elif menuInput == 3:
+        print("See you later!")
+        validInput = True
+
+    else:
+        print("That is not an option, please try again.")
---
+++ textAdventure/saveData.py
@@ -0,0 +1,4 @@
+
+def loadGame():
+
+    print("Game loaded!")
